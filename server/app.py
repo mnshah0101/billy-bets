09be30bb9fdc4e8596be1d0f014c49efc7fbee01
ronitjain10 <ronitrjain@berkeley.edu>
2024-02-24 19:01:25 -0800

    updated server

--- server/app.py
+++ server/app.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 from models.subjective import get_subjective_answer
+from models.objective import get_objective_answer
+from models.chat import get_answer
 from flask import Flask, request, jsonify
 import sys
 sys.path.insert(0, '..')
@@ -14,6 +16,24 @@ def ask_subjective():
     # Replace this with the code to process the question and generate a response.
     answer = get_subjective_answer(question)
     return jsonify({'question': question, 'answer': answer})
+
+
+@app.route('/askObjective')
+def ask_objective():
+    question = request.args.get('question', '')
+    # Replace this with the code to process the question and generate a response.
+    answer = get_objective_answer(question)
+    return jsonify({'question': question, 'answer': answer})
+
+@app.route('/chat')
+def chat(): 
+    question = request.args.get('question', '')
+    # Replace this with the code to process the question and generate a response.
+    answer = chat(question)
+    return jsonify({'question': question, 'answer': answer})
+
+
+
 
 
 if __name__ == '__main__':
